75a499bd4fa11c079c8a78902de41c668f3ef3bf
Stephan van Geffen <159128069+PookiebearStephan@users.noreply.github.com>
2025-03-24 12:12:07 +0100

    ?

--- obj/Release/net8.0-windows/WhackAMole.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/WhackAMole.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhackAMole")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fefad074e53ef661361e47c3e5afe2c54fe7b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhackAMole")]
